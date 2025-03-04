a0a485e54e6ed7c3ee741244dd50d987dfd123a8
vector341 <1786816365@qq.com>
2025-03-04 23:18:54 +0800

    Initial vuepress@v1
    使用 vuepress@v1 重构博客

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "vuepress/config";
+export default defineConfig({
+  base: "/vector341.github.io/",
+  title: "vector 的博客",
+  description: "...",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "vector 的博客",
+        items: [
+          {
+            text: "Github",
+            link: "https://github.com/vector341",
+          },
+          {
+            text: "掘金",
+            link: "https://juejin.cn/user/",
+          },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        title: "template",
+        path: "/",
+        collapsable: false,
+        children: [{ title: "Test Title", path: "/" }],
+      },
+      {
+        title: "从 ECMA 规范看 JavaScript",
+        path: "/javascript/2025-03-02-this-from-ecma-spec",
+        children: [
+          {
+            title: "ECMA-262",
+            path: "/javascript/2025-03-02-this-from-ecma-spec",
+          },
+        ],
+      },
+    ],
+  },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+});
